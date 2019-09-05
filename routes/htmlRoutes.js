c5592e07ad1d3e937cbb3157234905faf15f666a
Lindsay Chapin <lnchapin@gmail.com>
2019-09-05 01:09:56 -0600

    sending pet info

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth')
 const db = require('../models');
 const Users = db.User;
-const Posts = db.Posts;
+const Pets = db.Pet;
 
 router.get('/', forwardAuthenticated, (req, res) => res.render('index'))
 
@@ -12,9 +12,22 @@ router.get('/login', forwardAuthenticated, (req, res) => res.render('login'))
 router.get('/signup', forwardAuthenticated, (req, res) => res.render('signup'))
 
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('dashboard', {
-    user: req.user
+  Users.findByPk(req.user.id, {include:[{ model: Pets }]
   })
+  .then( info =>{
+    let currentPet;
+    for (var i = 0; i < info.Pets.length; i++) {
+      if(info.Pets[i].active){
+        currentPet = info.Pets[i]
+      }
+    }
+    hbsObject = { user: info, pet: currentPet }
+    // res.send(hbsObject)
+    return res.render("dashboard", hbsObject);
+  }
+  )
+  .catch(err=>console.log(err))
+
 );
 router.get('/test', ensureAuthenticated, (req, res) =>
   res.render('testing', {
